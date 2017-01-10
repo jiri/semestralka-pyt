17e0d13c741c57390774a7476b86616d3e43fb34
Jiří Šebele <sindriava@gmail.com>
2017-01-10 13:03:18 +0100

    :skull: Add a project skeleton

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 import click
 
-@click.command()
-def hello():
-    click.echo("Hello, world!")
+@click.group()
+def main():
+    pass
+
+# inverzní obraz
+@main.command(help = "Color inversion")
+def invert():
+    pass
+
+# převod do odstínů šedi
+@main.command(help = "Color removal")
+def greyscale():
+    pass
+
+# zesvětlení/ztmavení
+@main.command(help = "Increase image brightness")
+@click.option('--amount', default = 1, help = "Number of steps to apply")
+def dodge(amount):
+    pass
+
+@main.command(help = "Decrease image brightness")
+@click.option('--amount', default = 1, help = "Number of steps to apply")
+def burn(amount):
+    pass
+
+# zvýraznění hran
+@main.command(help = "Edge detection filter")
+def edges():
+    pass
 
 if __name__ == '__main__':
-    hello()
+    main()
